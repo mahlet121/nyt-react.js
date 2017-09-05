b287e94855049389d3b54487e9c88ce30f6336af
unknown <mahleta333@gmail.com>
2017-09-04 23:25:26 -0500

    upgrade npm

--- app/app.js
+++ app/app.js
@@ -2,5 +2,5 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var routes = require('./config/routes');
-
+// To display the result take routes
 ReactDOM.render(routes, document.getElementById('root'));
